b738154969f2ecc1a42cd1cfaa4f5199a2b7e773
Benjamin Olumuyiwa <benjaminsqlserver@gmail.com>
2024-04-25 11:05:56 +0100

    first commit

---
+++ Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace Catalog.API
+{
+    public class Startup
+    {
+        private  IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            
+        }
+
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            
+            }
+            app.UseRouting();
+            app.UseStaticFiles();
+         
+           
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllers();
+               
+            }
+            );
+
+         
+        }
+    }
+}
